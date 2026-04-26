3012ba9e6013217b9fa1623d139ccb1c51ce4cb9
LvuhaCool <ghabaruk@gmail.com>
2026-04-26 21:33:20 +0300

    fix progress bar, not finish its functionality

--- ts-dist/script.js
+++ ts-dist/script.js
@@ -1,5 +1,7 @@
 "use strict";
-const scoreNum = document.querySelector('.score__number'), capybaraImg = document.querySelector('.img img'), clickAudio = new Audio('./../audio/click.mp3');
+var _a, _b;
+const scoreNum = document.querySelector('.score__number'), capybaraImg = document.querySelector('.img img'), clickAudio = new Audio('./../audio/click.mp3'), normalVelocityBarValue = (_a = document.querySelector('.normal-speed')) === null || _a === void 0 ? void 0 : _a.getAttribute('value'), doubleVelocityBarValue = (_b = document.querySelector('.double-speed')) === null || _b === void 0 ? void 0 : _b.getAttribute('value'), normalVelocityBarMaxProgress = 2, doubleVelocityMaxProgress = 1, bestClickInterval = 125;
+let timestamps = [];
 clickAudio.volume = 0.15;
 document.addEventListener('DOMContentLoaded', () => {
     let reloadScore = localStorage.getItem('score');
@@ -21,6 +23,13 @@ capybaraImg.addEventListener('click', () => {
     let currentScore = localStorage.getItem('score');
     let currentScoreAsNum = Number(currentScore);
     let newScore = currentScoreAsNum + 1;
+    const now = Date.now();
+    timestamps.unshift(now);
+    if (timestamps.length === 2) {
+        const velocity = timestamps[0] - timestamps[1];
+        const velocityQuality = Number((1 / (velocity / 125)).toFixed(2));
+        console.log(velocityQuality);
+    }
     let newScoreAsString = newScore.toString();
     scoreNum.textContent = newScoreAsString;
     localStorage.setItem('score', newScoreAsString);
--- ts-src/script.ts
+++ ts-src/script.ts
@@ -1,11 +1,19 @@
 const scoreNum = document.querySelector('.score__number') as HTMLElement,
   capybaraImg = document.querySelector('.img img') as HTMLElement,
-  clickAudio: HTMLAudioElement = new Audio('./../audio/click.mp3');
+  clickAudio: HTMLAudioElement = new Audio('./../audio/click.mp3'),
+  normalVelocityBarValue = document.querySelector('.normal-speed')?.getAttribute('value') as string,
+  doubleVelocityBarValue = document.querySelector('.double-speed')?.getAttribute('value') as string,
+  normalVelocityBarMaxProgress: number = 2,
+  doubleVelocityMaxProgress: number = 1,
+  bestClickInterval: number = 125;
+
+let timestamps: number[] = [];
 
 clickAudio.volume = 0.15;
 
 document.addEventListener('DOMContentLoaded', (): void => {
     let reloadScore: string | null = localStorage.getItem('score');
+
     if (!reloadScore) {
         scoreNum.textContent = '0';
         localStorage.setItem('score', '0');
@@ -17,14 +25,28 @@ document.addEventListener('DOMContentLoaded', (): void => {
 
 capybaraImg.addEventListener('click', (): void => {
     clickAudio.play();
+
     const clickEffect: string = 'click-effect'
     capybaraImg.classList.add(clickEffect);
     setTimeout((): void => {
         capybaraImg.classList.remove(clickEffect);
     }, 55);
+
     let currentScore = localStorage.getItem('score') as string;
     let currentScoreAsNum: number = Number(currentScore);
     let newScore: number = currentScoreAsNum + 1;
+
+    const now: number = Date.now();
+    timestamps.unshift(now);
+    
+
+    if (timestamps.length === 2) {
+        const velocity: number = timestamps[0] - timestamps[1];
+        const velocityQuality: number = Number((1 / (velocity / 125)).toFixed(2));
+        console.log(velocityQuality);
+        
+    }
+
     let newScoreAsString: string = newScore.toString();
     scoreNum.textContent = newScoreAsString;
     localStorage.setItem('score', newScoreAsString);
